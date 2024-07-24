97ad367180e8d019ce865568a43dc05c888582f5
DanielBieer <danibierdasilva@gmail.com>
2024-07-24 20:11:46 -0300

    feature: Criando um contexto de contador

---
+++ src/contexts/contador.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import { useState } from "react";
+import { createContext } from "react";
+
+const ContadorContext = createContext(
+    {
+        contador: 0,
+        incrementar: () => {},
+        decrementar: () => {},
+    }
+)
+
+export function ContadorProvider({ children }){
+
+const [contador, setContador] = useState(0)
+
+function incrementar(){
+    setContador(prev => prev +1)
+}
+
+
+function decrementar(){
+    setContador(prev => prev -1)
+}
+
+    return <ContadorContext.Provider value={{ contador, decrementar, incrementar}}>
+        {children}
+    </ContadorContext.Provider>
+}
+
+export function useContador(){
+    const contexto = useContext(ContadorContext)
+
+    return contexto
+}
